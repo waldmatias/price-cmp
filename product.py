a26fa77042e498a9e732803960114d4d66c77233
waldmatias <waldmatias@hotmail.com>
2020-10-16 23:35:09 -0400

    refactoring

--- product.py
+++ product.py
@@ -8,6 +8,7 @@ def open_url(url):
     """
     return BeautifulSoup(urlopen(url),'html.parser')
 
+
 def parse_rate(text):
     if type(text) is float:
         return D(text).quantize(D('1.00'))
@@ -35,7 +36,9 @@ def get_db():
             'name' : 'Excelsior Gama',
             'base_url' : 'https://compraenlinea.excelsiorgama.com/p/{0}',
             'products' : {
-                'nevada-5l': {'supplier_id': '10000746'},
+                'nevada-5l': {
+                    'supplier_id': '10000746'
+                },
             },
             'desc-parser': lambda page: page.find('div', {'class':'name'}).text.strip(),
             'price-parser': lambda page: convert_price('Bs', page.find('div', {'class':'from-price-value'}).text),
@@ -48,7 +51,9 @@ def get_db():
                     'supplier_id': '10003139', 
                     'store_id' : '1013'
                 }
-            }
+            },
+            'desc-parser': lambda page: page.find('div', {'class':'ProductName'}).text.strip(),
+            'price-parser': lambda page: D((page.select('span#productprice.Moneda')[-1].text).replace(',' , '')),  #digit formatting different ,.,
         },
         'cm' : {
             'name' : 'Central Madeirense',
@@ -58,7 +63,9 @@ def get_db():
                     'supplier_id': 'agua-nevada-5lt', 
                     'store_id' : 'distrito-capital-06'
                 }
-            }
+            },
+            'desc-parser': lambda page: page.select('h2.product_title')[-1].text.strip(),
+            'price-parser': lambda page: convert_price('Bs', page.select('p.price bdi')[-1].text)
         },
         'farmatodo' : {
             'name' : 'Farmatodo',
@@ -67,82 +74,55 @@ def get_db():
                 'nevada-5l': {
                     'supplier_id': '111240637'
                 }, 
-            }
+            },
+            'desc-parser': lambda page: page.find('p', {'class':'description'}).text.strip(), 
+            'price-parser': lambda page: convert_price('Bs.', page.find('p', {'class':'p-blue'}).text.strip())
         },
     }
 
 
-def get_db_product(db, source_name, key):
-    source = db.get(source_name)
-    product = source.get('products').get(key)
-    return product
-
-
-def fetch_product(db, source_name, product_key, store_key):
-    product = get_db_product(db, source_name, product_key)
-    pid = product.get('supplier_id')
-    base_url = db.get(source_name).get('base_url')
+def fetch_product(source, product_key, store_key):
+    product = source['products'][product_key]
+    pid = product['supplier_id']
+    base_url = source['base_url']
     return open_url(base_url.format(pid, store_key))
 
 
-def fetch_products():
+def fetch_products(product_key):
     info = []
 
     db = get_db()
-    product_key = 'nevada-5l'
 
-    # gama
-    src = 'gama'
-    # source, description, price = fetch_product(db, src, 'nevada-5l')
-    # info.append([source, description, price])
-    s = fetch_product(db, src, product_key, None)
-    info.append([
-        src, db[src]['desc-parser'](s), db[src]['price-parser'](s)        
-    ])
+    sites = [
+        ('gama', None),
+        ('plazas','1013'),
+        ('cm','distrito-capital-06'),
+        ('farmatodo', None)
+    ]
 
-    # plaza
-    src = 'plazas'
-    store = '1013' #centro plaza
-    s = fetch_product(db, src, product_key, store)
-    # s = open_url(db['plazas']['base_url'].format(pid, suc))
-    info.append([
-        src,
-        s.find('div', {'class':'ProductName'}).text.strip(), 
-        D((s.select('span#productprice.Moneda')[-1].text).replace(',' , ''))  #digit formatting different ,.
-    ])
-
-    # cm
-    src = 'cm'
-    store = 'distrito-capital-06'
-    s = fetch_product(db, src, product_key, store)
-    info.append([
-        src, 
-        s.select('h2.product_title')[-1].text.strip(), 
-        convert_price('Bs', s.select('p.price bdi')[-1].text)
-    ])
-
-    # farmatodo
-    src = 'farmatodo'
-    s = fetch_product(db, src, product_key, None)
-    info.append([
-        src, 
-        s.find('p', {'class':'description'}).text.strip(), 
-        convert_price('Bs.', s.find('p', {'class':'p-blue'}).text.strip())
-    ])
+    for site, store in sites:
+        src = db[site]
+        wwwpage = fetch_product(src, product_key, store)
+        info.append([
+            src['name'], src['desc-parser'](wwwpage), src['price-parser'](wwwpage)
+        ])
 
     return info
 
+
 def print_view(product_list):
     for source, product, price in product_list:
-        print(f'{source.upper():>10} : {product:<40} : Bs. {price:<10,.2f}')
+        print(f'{source.upper():>20} : {product:<40} : Bs. {price:<10,.2f}')
     
     # item[2] is price
     source, _, price = min(product_list, key=lambda item: item[2])
-    print('-' * 10)
-    print(f'cheapest price is at: {source.upper()} and its price is: Bs. {price:,.2f}')
+    print('-' * 20)
+    print(f'cheapest price at: {source.upper()} and is: Bs. {price:,.2f}')
 
 
 if __name__ == '__main__':
-    print(f'Fetching prices ...')
-    products = fetch_products()
+    product_key = 'nevada-5l'
+
+    print(f'Fetching prices for {product_key} ...')
+    products = fetch_products(product_key)
     print_view(products)
