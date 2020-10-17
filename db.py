0e5de4cd2120265c61cc9e584c0d5f79cd0f0e4f
waldmatias <waldmatias@hotmail.com>
2020-10-17 01:21:22 -0400

    refactoring

--- db.py
+++ db.py
@@ -1,27 +1,19 @@
 from decimal import Decimal as D
 from parseutils import convert_price
 
-def get_db():
+def get_db_products():
     return {
-        'gama' : {
-            'name' : 'Excelsior Gama',
-            'base_url' : 'https://compraenlinea.excelsiorgama.com/p/{0}',
-            'desc-parser': lambda page: page.find('div', {'class':'name'}).text.strip(),
-            'price-parser': lambda page: convert_price('Bs', page.find('div', {'class':'from-price-value'}).text),
-            'products' : {
+        'gama': {
+            'products': {
                 'nevada-5l': {
                     'supplier_id': '10000746'
                 },
                 'arroz-mary-tradicional-kg': {
                     'supplier_id': '10029311'
                 },
-            },            
+            }
         },
         'plazas' : {
-            'name' : 'El Plazas',
-            'base_url' : 'https://www.elplazas.com/Product.php?code={0}&suc={1}',
-            'desc-parser': lambda page: page.find('div', {'class':'ProductName'}).text.strip(),
-            'price-parser': lambda page: D((page.select('span#productprice.Moneda')[-1].text).replace(',' , '')),  #digit formatting different ,.,
             'products' : {
                 'nevada-5l': {
                     'supplier_id': '10003139', 
@@ -31,13 +23,9 @@ def get_db():
                     'supplier_id': '16001524', 
                     'store_id' : '1013'
                 },
-            },            
+            },  
         },
         'cm' : {
-            'name' : 'Central Madeirense',
-            'base_url' : 'https://tucentralonline.com/{1}/producto/{0}/', #non-numeric, alpha-key
-            'desc-parser': lambda page: page.select('h2.product_title')[-1].text.strip(),
-            'price-parser': lambda page: convert_price('Bs', page.select('p.price bdi')[-1].text),
             'products' : {
                 'nevada-5l': {
                     'supplier_id': 'agua-nevada-5lt', 
@@ -47,13 +35,9 @@ def get_db():
                     'supplier_id': 'arroz-mary-tipo1tradicional-1kg', 
                     'store_id' : 'distrito-capital-06'
                 },
-            },            
+            },    
         },
         'farmatodo' : {
-            'name' : 'Farmatodo',
-            'base_url' : 'https://farmatodo.com.ve/producto/{0}',
-            'desc-parser': lambda page: page.find('p', {'class':'description'}).text.strip(), 
-            'price-parser': lambda page: convert_price('Bs.', page.find('p', {'class':'p-blue'}).text.strip()),
             'products' : { 
                 'nevada-5l': {
                     'supplier_id': '111240637'
@@ -62,5 +46,38 @@ def get_db():
                     'supplier_id': '112084350', 
                 },
             },
+        },
+    }
+
+
+def get_db():
+    return {
+        'gama' : {
+            'name' : 'Excelsior Gama',
+            'base_url' : 'https://compraenlinea.excelsiorgama.com/p/{0}',
+            'desc-parser': lambda page: page.find('div', {'class':'name'}).text.strip(),
+            'price-parser': lambda page: convert_price('Bs', page.find('div', {'class':'from-price-value'}).text),
+            'products' : get_db_products()['gama']['products'],
+        },
+        'plazas' : {
+            'name' : 'El Plazas',
+            'base_url' : 'https://www.elplazas.com/Product.php?code={0}&suc={1}',
+            'desc-parser': lambda page: page.find('div', {'class':'ProductName'}).text.strip(),
+            'price-parser': lambda page: D((page.select('span#productprice.Moneda')[-1].text).replace(',' , '')),  # digit formatting different ,.,
+            'products' : get_db_products()['plazas']['products'],
+        },
+        'cm' : {
+            'name' : 'Central Madeirense',
+            'base_url' : 'https://tucentralonline.com/{1}/producto/{0}/', # non-numeric, alpha-key
+            'desc-parser': lambda page: page.select('h2.product_title')[-1].text.strip(),
+            'price-parser': lambda page: convert_price('Bs', page.select('p.price bdi')[-1].text),
+            'products' : get_db_products()['cm']['products'],
+        },
+        'farmatodo' : {
+            'name' : 'Farmatodo',
+            'base_url' : 'https://farmatodo.com.ve/producto/{0}',
+            'desc-parser': lambda page: page.find('p', {'class':'description'}).text.strip(), 
+            'price-parser': lambda page: convert_price('Bs.', page.find('p', {'class':'p-blue'}).text.strip()),
+            'products' : get_db_products()['farmatodo']['products'],
         },
     }
