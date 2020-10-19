3bc2e89cc9aea93cb6c6179ecf95ce13f829131c
waldmatias <waldmatias@hotmail.com>
2020-10-19 07:12:03 -0400

    refactoring

--- db.py
+++ db.py
@@ -1,11 +1,12 @@
 from decimal import Decimal as D
 from parseutils import convert_price
 
-def get_db_products():
+def db_products():
+    # get these from a json file
     return {
         'gama': {
             'products': {
-                'nevada-5l': {
+                'nevada-5l': { 
                     'supplier_id': '10000746'
                 },
                 'arroz-mary-tradicional-kg': {
@@ -64,34 +65,40 @@ def get_db_products():
     }
 
 
-def get_db():
+def db_sources():
     return {
         'gama' : {
             'name' : 'Excelsior Gama',
             'base_url' : 'https://compraenlinea.excelsiorgama.com/p/{0}',
             'desc-parser': lambda page: page.find('div', {'class':'name'}).text.strip(),
             'price-parser': lambda page: convert_price('Bs', page.find('div', {'class':'from-price-value'}).text),
-            'products' : get_db_products()['gama']['products'],
         },
         'plazas' : {
             'name' : 'El Plazas',
             'base_url' : 'https://www.elplazas.com/Product.php?code={0}&suc={1}',
             'desc-parser': lambda page: page.find('div', {'class':'ProductName'}).text.strip(),
             'price-parser': lambda page: D((page.select('span#productprice.Moneda')[-1].text).replace(',' , '')),  # digit formatting different ,.,
-            'products' : get_db_products()['plazas']['products'],
         },
         'cm' : {
             'name' : 'Central Madeirense',
             'base_url' : 'https://tucentralonline.com/{1}/producto/{0}/', # non-numeric, alpha-key
             'desc-parser': lambda page: page.select('h2.product_title')[-1].text.strip(),
             'price-parser': lambda page: convert_price('Bs', page.select('p.price bdi')[-1].text),
-            'products' : get_db_products()['cm']['products'],
         },
         'farmatodo' : {
             'name' : 'Farmatodo',
             'base_url' : 'https://farmatodo.com.ve/producto/{0}',
             'desc-parser': lambda page: page.find('p', {'class':'description'}).text.strip(), 
             'price-parser': lambda page: convert_price('Bs.', page.find('p', {'class':'p-blue'}).text.strip()),
-            'products' : get_db_products()['farmatodo']['products'],
         },
     }
+
+def get_db():
+    # data sets
+    sources = db_sources()
+    products = db_products()
+
+    for source in sources:
+        sources[source].update(products[source])
+
+    return sources
